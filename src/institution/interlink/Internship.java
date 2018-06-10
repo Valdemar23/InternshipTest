ac21b15f64230da64cb3081600c90a22c86b8a40
HP <darthrvader1648@gmail.com>
2018-06-11 01:32:08 +0300

    CaP5

--- src/institution/interlink/Internship.java
+++ src/institution/interlink/Internship.java
@@ -1,14 +1,19 @@
 package institution.interlink;
 
 import person.Student;
+import person.consciousness.Knowledge;
 
 public class Internship {
-
+    private String nameIntership;
+    private Student student;
+    private Knowledge knowledge;
     public Internship(String name) {
+        nameIntership=name;
         //TODO: Implementation is needed
     }
 
     public void setStudent(Student student) {
+        this.student=student;
         //TODO: Implementation is needed
     }
 
@@ -16,4 +21,6 @@ public class Internship {
         //TODO: Implementation is needed
         return "Andrew Maslenko\nJulia Veselkina\n";
     }
+
+    
 }
--- src/person/Student.java
+++ src/person/Student.java
@@ -17,4 +17,6 @@ public class Student {
     public Knowledge getKnowledge(){
         return personKnowledge;
     }
+
+
 }
